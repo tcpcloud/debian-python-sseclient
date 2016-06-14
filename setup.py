5f02783154533f255dde0743861685d9483e6bb1
Filip Pytloun <filip@pytloun.cz>
2016-06-14 19:40:36 +0200

    Imported Upstream version 0.0.12

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+from setuptools import setup
+
+needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
+pytest_runner = ['pytest_runner>=2.1'] if needs_pytest else []
+
+setup(
+    name='sseclient',
+    version='0.0.12',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    py_modules=['sseclient'],
+    install_requires=['requests>=2.0.0', 'six'],
+    tests_require=['pytest', 'backports.unittest_mock'],
+    setup_requires=[] + pytest_runner,
+    description=(
+        'Python client library for reading Server Sent Event streams.'),
+    long_description=open('README.rst').read(),
+    url='https://bitbucket.org/btubbs/sseclient/',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+)
